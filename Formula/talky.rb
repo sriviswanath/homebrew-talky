b78c974a8007c371e1c7d1b251070d673095fea8
Sri Viswanath <sri@sycamore.so>
2026-06-25 07:26:57 -0700

    Update Talky formula to 0.1.0-952-g7d8bac4d

--- Formula/talky.rb
+++ Formula/talky.rb
@@ -9,16 +9,16 @@
 class Talky < Formula
   desc "Talky CLI + local-gateway daemon (the laptop Box Gateway)"
   homepage "https://talky.so"
-  version "0.1.0-935-g2a862866"
+  version "0.1.0-952-g7d8bac4d"
 
   on_macos do
     on_arm do
-      url "https://dl.talky.so/v0.1.0-935-g2a862866/talky_0.1.0-935-g2a862866_darwin_arm64.tar.gz"
-      sha256 "b3c4be829cdb778a567325b5246cf0b1df10eccbc31009342de91d2e026c0ab2"
+      url "https://dl.talky.so/v0.1.0-952-g7d8bac4d/talky_0.1.0-952-g7d8bac4d_darwin_arm64.tar.gz"
+      sha256 "fdf2cd7dfc3b37069092357325d95d1554db9f2b476fdc0e1052ab60d22f086e"
     end
     on_intel do
-      url "https://dl.talky.so/v0.1.0-935-g2a862866/talky_0.1.0-935-g2a862866_darwin_amd64.tar.gz"
-      sha256 "e522534c320d9b58aef4b30d92b8e4b68621b39a997bedf953e305b1a16f41f5"
+      url "https://dl.talky.so/v0.1.0-952-g7d8bac4d/talky_0.1.0-952-g7d8bac4d_darwin_amd64.tar.gz"
+      sha256 "806b3338a09395d57631fc2863d80ece5ad0dda7d60c1d8418c7b1045aa2d75e"
     end
   end
 
